0c7070be2a010ff519bb83cc5f6d1375126b3c09
Klas Lindgren <klslndgrn@gmail.com>
2022-04-30 12:39:33 +0200

    Updates

--- PowerGrid/psclasses.py
+++ PowerGrid/psclasses.py
@@ -4,22 +4,35 @@ class PSEquipment():
     Connectivity Nodes and Conducting Equipment.
     Conducting Equipment is
     '''
-    def __init__(self):
-        pass
+    def __init__(self, ID, Name, Type='NotSpecified', Processed=False):
+
+        self.ID = ID
+        self.Name = Name
+        self.Type = Type
+
+        self.Processed = Processed
+
+    def __repr__(self):
+        return(f'({self.Name}, Type = {self.Type}, \
+               Processed = {self.Processed}) \n')
 
 
 class Terminal(PSEquipment):
     '''
     A subclass to PSEquipment that creates Terminal variables.
     '''
-    def __init__(self, ID, ConductingEquipmentID, ConnectivityNodeID,
-                 Name=None, ConnectionStatus=True, Processed=False):
+    def __init__(self, ID, Name, ConductingEquipmentID, ConnectivityNodeID,
+                 Type='NotSpecified', ConnectionStatus=True, Processed=False):
         super().__init__()
+
         self.ID = ID
+        self.Name = Name
+        self.Type = Type
+
         self.CE = ConductingEquipmentID
         self.CN = ConnectivityNodeID
-        self.Name = Name
         self.ConStatus = ConnectionStatus
+
         self.Processed = Processed
 
     def __repr__(self):
@@ -31,13 +44,16 @@ class ConnectivityNode(PSEquipment):
     '''
     A subclass to PSEquipment that creates ConnectivityNode variables.
     '''
-
-    def __init__(self, ID, ConnectivityNodeContainerID, Name=None,
-                 Processed=False):
+    def __init__(self, ID, Name, ConnectivityNodeContainerID,
+                 Type='NotSpecified', Processed=False):
         super().__init__()
+
         self.ID = ID
-        self.CID = ConnectivityNodeContainerID
         self.Name = Name
+        self.Type = Type
+
+        self.CID = ConnectivityNodeContainerID
+
         self.Processed = Processed
 
     def __repr__(self):
@@ -50,8 +66,16 @@ class ConductingEquipment(PSEquipment):
     A subclass to PSEquipment that creates ConductingEquipment variables.
     There are multiple of subclasses to this subclass.
     '''
-    def __init__(self):
-        pass
+    def __init__(self, ID, Name, CE_Type, Type='NotSpecified',
+                 Processed=False):
+
+        self.ID = ID
+        self.Name = Name
+        self.Type = Type
+
+        self.CE_Type = CE_Type
+
+        self.Processed = Processed
 
 
 class ConnectivityNodeGroup():
