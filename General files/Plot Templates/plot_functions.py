dfb2ffc9be76b7b2ffa136aa4c70a105575e2a32
Klas Lindgren <klslndgrn@gmail.com>
2022-09-29 14:33:43 +0200

    Updating plotting functions

---
+++ General files/Plot Templates/plot_functions.py
@@ -0,0 +1,118 @@
+# Plot functions
+
+"""
+Linestyles as:
+    linestyle =
+        'solid' same as '-'
+        'dotted' same as ':'
+        'dashed' same as '--'
+        'dashdot' same as '-.'
+Base-colors as:
+    color =
+        'b' as blue
+        'g' as green
+        'r' as red
+        'c' as cyan (blue-green)
+        'm' as magenta (pink)
+        'y' as dark yellow
+        'k' as black
+        'w' as white
+RBG Colors as:
+    color =
+        'tab:blue'
+        'tab:orange'
+        'tab:green'
+        'tab:red'
+        'tab:purple'
+        'tab:brown'
+        'tab:pink'
+        'tab:gray'
+        'tab:olive'
+        'tab:cyan'
+Linewidth as:
+    linewidth = > 0 "larger than 0!"
+Alpha of line as:
+    alpha = 0 <-> 1
+"""
+
+
+def plot_one_graph(Variables, Functions,
+                   Fignum='', Title='TITLE',
+                   XLabel='X-label', YLabel='Y-label',
+                   XLimits=[], YLimits=[],
+                   XLines=[], YLines=[],
+                   LineLabels=None, LineColors=None, LineStyles=None,
+                   SaveAs=None):
+    '''This function uses Matplotlib to plot.
+    Atleast one X-vector and Y-vector is needed.'''
+
+    # ---- Libraries ---- # ---------------------------------------------------
+    import matplotlib.pyplot as plt
+    from matplotlib import rc
+
+    # ---- Settings ---- # ----------------------------------------------------
+    rc('text', usetex=True)
+    rc('font', size=10)
+    rc('legend', fontsize=12)
+
+    # ---- Figure ---- # ------------------------------------------------------
+    plt.figure(f'PLOT {Fignum}', figsize=(10, 7))
+    # ---- PLOTS ---- # -------------------------------------------------------
+    if LineLabels is not None and LineColors is None:
+        for x, y, l in zip(Variables,
+                           Functions,
+                           LineLabels):
+            plt.plot(x, y, label=l, linewidth=1)
+    elif LineLabels is not None and LineColors is not None:
+        if LineStyles is not None:
+            for x, y, l, c, t in zip(Variables,
+                                     Functions,
+                                     LineLabels,
+                                     LineColors,
+                                     LineStyles):
+                plt.plot(x, y, label=l, color=c, linestyle=t, linewidth=1)
+        else:
+            for x, y, l, c in zip(Variables,
+                                  Functions,
+                                  LineLabels,
+                                  LineColors):
+                plt.plot(x, y, label=l, color=c, linewidth=1)
+    else:
+        for x, y in zip(Variables, Functions):
+            plt.plot(x, y, linewidth=1)
+    # ---- X-LINE PLOTS ---- # ------------------------------------------------
+    if XLines is not None:
+        for x in XLines:
+            plt.axvline(x=x, color='k', linestyle='--', linewidth=1)
+    # ---- Y-LINE PLOTS ---- # ------------------------------------------------
+    if YLines is not None:
+        for y in YLines:
+            plt.axhline(y=y, color='k', linestyle='--', linewidth=1)
+    # ---- X AND Y AXIS ---- # ------------------------------------------------
+    plt.axhline(y=0, color='k', linewidth=0.9, alpha=0.3)  # X-AXIS
+    plt.axvline(x=0, color='k', linewidth=0.9, alpha=0.3)  # Y-AXIS
+    # ---- AXIS ---- # --------------------------------------------------------
+    if len(XLimits) == 2:
+        plt.xlim(XLimits)  # XLIM
+    if len(YLimits) == 2:
+        plt.ylim(YLimits)  # YLIM
+    # ---- FORMATTING ---- # --------------------------------------------------
+    plt.grid(visible=True, which='major', color='k', linestyle='-', alpha=0.2)
+    plt.grid(visible=True, which='minor', color='k', linestyle='--', alpha=0.1)
+    plt.minorticks_on()
+    # ---- LEGEND ---- # ------------------------------------------------------
+    if LineLabels is not None:
+        plt.legend()
+    # ---- LABELS ---- # ------------------------------------------------------
+    plt.title(Title, fontsize=14)
+    plt.xlabel(XLabel, fontsize=12)
+    plt.ylabel(YLabel, fontsize=12)
+    # ---- SAVE PLOT ---- # ---------------------------------------------------
+    if SaveAs is not None:
+        if 'png' in SaveAs:
+            plt.savefig(SaveAs, dpi=600)
+        else:
+            plt.savefig(SaveAs)
+    # ---- SHOW PLOT ---- # ---------------------------------------------------
+    plt.show()
+    # -------------------------------------------------------------------------
---
+++ General files/Plot Templates/plot_test.py
@@ -0,0 +1,22 @@
+import numpy as np
+from plot_functions import plot_one_graph
+
+# ---- EXAMPLE DATA FOR THE PLOT ---- #
+num_points = 1000
+x = np.linspace(-4*np.pi, 4*np.pi, num=num_points)
+f1 = np.exp(x/10)*np.sin(x)*np.cos(x)
+f2 = np.exp(2*x/10)*np.sin(2*x)*np.cos(2*x)
+
+x_ax_lim = [-10, 10]  # X-limits
+y_ax_lim = [-5, 5]  # Y-limits
+
+lbls = ['$f_1$', '$f_2$']
+
+plot_one_graph([x, x], [f1, f2],
+               LineLabels=lbls,
+               LineColors=['r', 'b'],
+               LineStyles=['-', '--'],
+               XLimits=x_ax_lim,
+               YLimits=y_ax_lim)
+
+# , SaveAs='test.png'
